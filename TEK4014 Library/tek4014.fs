8ea68447f5c5dd9f9e7374ab91edd06e84b51d08
unknown <igrmks@gmail.com>
2018-01-30 17:42:08 +0100

    Clean-up

--- TEK4014 Library/tek4014.fs
+++ TEK4014 Library/tek4014.fs
@@ -6,34 +6,34 @@
 \ For more information search the "TEK4010/4014 Graphics Vector Terminal"
 \
 \ Provided as-is
-\ No no warranties of any kind are provided
+\ No warranties of any kind are provided
 \ by IgorM 30 Jan 2018
 
 
-: tekcls ( -- )  					\ Clean the Tek window
+: tekcls ( -- )                     \ Clean the Tek window
 		$1b emit $0c emit ;
 		
-: tekcolor ( color -- )  			\ Select a line/dot/mark color [0-7]
+: tekcolor ( color -- )             \ Select a line/dot/mark color [0-7]
 		$1b emit $5b emit 
 		$33 emit $30 + emit 
 		$6d emit ;
 		
-: tekgraph ( style -- ) 			\ Select graphics mode with line style [0-4]
+: tekgraph ( style -- )             \ Select graphics mode with line style [0-4]
 		$1d emit $1b emit $60 or emit ;
 		
-: tekfont ( size -- ) 				\ Select font size [0-3]
+: tekfont ( size -- )               \ Select font size [0-3]
 		$1b emit $38 + emit ;
 		
-: tekstyle ( style -- ) 			\ Select line style [0-15]
+: tekstyle ( style -- )             \ Select line style [0-15]
 		$1b emit $60 + emit ;
 		
-: tekbstyle ( bstyle -- ) 			\ Select bold line style [??]
-		$1b emit $68 + emit ;		
+: tekbstyle ( bstyle -- )           \ Select bold line style [??]
+		$1b emit $68 + emit ;
 		
-: tekalpha ( -- )					\ Switch to the alphanumeric mode
+: tekalpha ( -- )                   \ Switch to the alphanumeric mode
 		$1f emit ;
 		
-: tekplot ( x y -- )				\ Continue to draw towards X Y
+: tekplot ( x y -- )                \ Continue to draw towards X Y
 		dup 
 		5 rshift $1f and $20 + emit
 		$1f and $60 + emit
@@ -41,11 +41,11 @@
 		5 rshift $1f and $20 + emit
 		$1f and $40 + emit ;
 		
-: tekmark ( x y -- ) 				\ Draw a small cross/mark at X Y
+: tekmark ( x y -- )                \ Draw a small cross/mark at X Y
 		$1c emit 
 		tekplot ;
 		
-: tekdot  ( x y -- ) 				\ Draw a single dot at X Y
+: tekdot  ( x y -- )                \ Draw a single dot at X Y
 		$1d emit
 		dup 
 		5 rshift $1f and $20 + emit
@@ -55,21 +55,21 @@
 		$1f and $40 + emit 
 		$1f and $40 + emit ;
 		
-: tekline ( x1 y1 x2 y2 -- ) 	 	\ Draw a line from X1 Y1 to X2 Y2
+: tekline ( x1 y1 x2 y2 -- )        \ Draw a line from X1 Y1 to X2 Y2
 		$1d emit
 		tekplot
 		tekplot ;
 		
-: teklocate ( x y -- )				\ Locate the alpha string origin
-		$1d emit					\ and switch to alpha mode
+: teklocate ( x y -- )              \ Locate the alpha string origin
+		$1d emit                    \ and switch into the alpha mode
 		tekplot
 		tekalpha ;
 	
-: teksprite ( addr u -- )			\ Draw a "sprite" (sprite is coded inside a string)
+: teksprite ( addr u -- )           \ Draw a "sprite" (sprite is coded inside a string)
 		$1e emit
 		type ; 
 		
-: tekgin ( -- ) 					\ Go to the Grapics-In mode
+: tekgin ( -- )                     \ Go to the Grapics-In mode
 		$1b emit
 		$1a emit ;
 
